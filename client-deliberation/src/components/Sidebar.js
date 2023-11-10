c650852cdd9740f8a60760a104c179ba99eb443c
JanSteinhauer <insta.steini14@gmail.com>
2023-11-10 11:25:23 -0600

    Sidebar Style Update

--- client-deliberation/src/components/Sidebar.js
+++ client-deliberation/src/components/Sidebar.js
@@ -4,12 +4,12 @@ import Module from './SidebarModule'; // Adjust the import path as needed
 const Sidebar = ({ modules, onModuleClick }) => {  const sidebarStyles = {
     display: 'flex',
     flexDirection: 'column',
-    width: '250px',
+    width: '300px',
     height: '100%',
     position: 'fixed',
     left: 0,
     top: 0,
-    backgroundColor: '#F4511E',
+    backgroundColor: '#cf5e00',
     color: '#FFF',
     padding: '10px',
     boxSizing: 'border-box',
@@ -25,7 +25,7 @@ const Sidebar = ({ modules, onModuleClick }) => {  const sidebarStyles = {
     <div style={sidebarStyles}>
       <h1 style={headingStyles}>Poll Tutorial</h1>
       {modules.map((module, index) => (
-        <div key={index} onClick={() => onModuleClick(index, false)} style={{ cursor: 'pointer', margin: '10px 0' }}>
+        <div key={index} onClick={() => onModuleClick(index, false)} style={{ cursor: 'pointer', margin: '10px 0', marginRight: '10px', marginLeft: '10px'}}>
           <Module name={module.name} progress={module.progress} currently_displayed={module.currently_displayed} not_completed={module.not_completed} />
         </div>
       ))}
--- client-deliberation/src/components/SidebarModule.js
+++ client-deliberation/src/components/SidebarModule.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Lock from './Lock';// Adjust the import path as needed
-
+import Lock from './Lock'; // Adjust the import path as needed
 
 const SidebarModule = ({ name, progress, currently_displayed, not_completed }) => {
   const moduleStyles = {
@@ -9,27 +8,47 @@ const SidebarModule = ({ name, progress, currently_displayed, not_completed }) =
     background: '#FFF',
     color: '#333',
     borderRadius: '4px',
-    boxShadow: '0 2px 4px rgba(0,0,0,0.1)'
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    height: '80px', // Fixed height for uniformity
+    display: 'flex', // Added for Flexbox layout
+    flexDirection: 'column', // Stack children vertically
+    justifyContent: 'center' // Center children vertically
   };
 
-  const fillerStyles = {
-    height: '100%',
-    width: `${progress}%`,
-    backgroundColor: '#F4511E',
-    borderRadius: 'inherit',
-    transition: 'width 1s ease-in-out'
+  const headerStyles = {
+    display: 'flex',
+    justifyContent: 'space-between', // Horizontally layout the header content
+    alignItems: 'center', // Vertically center the header content
+    width: '100%', // Take full width of the parent
   };
 
-  console.log("name ", name, "currently ", currently_displayed)
+  const textLockContainer = {
+    display: 'contents',
+    alignItems: 'center', // Vertically centers the text and lock icon
+  };
+
+  const textStyles = {
+    fontSize: '16px', // Consistent font size
+  };
+
+  const lockStyles = {
+    height: '30px', // Consistent height for the lock icon
+    marginLeft: '5px', // Space between text and lock icon
+  };
 
   return (
     <div style={moduleStyles}>
-      <div style={{ marginBottom: '5px', color: currently_displayed ? '#F4511E' : 'black'}}>{name} 
-      {' '}
-     {not_completed &&  <Lock style={{ fill: 'black' }} className="my-custom-class" />}
+      <div style={headerStyles}>
+        <div style={textLockContainer}>
+          <span style={{ ...textStyles, color: currently_displayed ? '#cf5e00' : 'black' }}>{name}</span>
+          <div style={{height:'25px'}}>
+            {not_completed && <Lock style={{ ...lockStyles, fill: 'black' }} className="my-custom-class" />}
+          </div>
+          
+        </div>
       </div>
       <div style={{ width: '100%', backgroundColor: '#e0e0e0', borderRadius: '4px' }}>
-        <div style={fillerStyles}></div>
+        <div style={{ height: '100%', width: `${progress}%`, backgroundColor: '#cf5e00', borderRadius: 'inherit', transition: 'width 1s ease-in-out' }}></div>
       </div>
     </div>
   );
